cd221d1f56d234733c1108d99f002709ee93eedc
evetn7 <35403848+evetn7@users.noreply.github.com>
2018-04-02 21:45:15 -0400

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class ClubForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.addActivity = new System.Windows.Forms.TextBox();
+            this.ClubOutinglabel = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.BxEvents = new System.Windows.Forms.ListBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addActivityLabel = new System.Windows.Forms.Label();
+            this.Addbutton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Gender = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Navy;
+            this.panel1.Controls.Add(this.ClubOutinglabel);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(416, 69);
+            this.panel1.TabIndex = 0;
+            // 
+            // addActivity
+            // 
+            this.addActivity.Location = new System.Drawing.Point(241, 255);
+            this.addActivity.Name = "addActivity";
+            this.addActivity.Size = new System.Drawing.Size(121, 26);
+            this.addActivity.TabIndex = 0;
+            this.addActivity.TextChanged += new System.EventHandler(this.addActivity_TextChanged);
+            // 
+            // ClubOutinglabel
+            // 
+            this.ClubOutinglabel.AutoSize = true;
+            this.ClubOutinglabel.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClubOutinglabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClubOutinglabel.Location = new System.Drawing.Point(65, 36);
+            this.ClubOutinglabel.Name = "ClubOutinglabel";
+            this.ClubOutinglabel.Size = new System.Drawing.Size(276, 23);
+            this.ClubOutinglabel.TabIndex = 1;
+            this.ClubOutinglabel.Text = "Computer Club Outing Sign Up";
+            this.ClubOutinglabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(0, 380);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(416, 26);
+            this.textBoxResult.TabIndex = 2;
+            this.textBoxResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxResult.TextChanged += new System.EventHandler(this.textBoxResult_TextChanged);
+            // 
+            // BxEvents
+            // 
+            this.BxEvents.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BxEvents.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.BxEvents.FormattingEnabled = true;
+            this.BxEvents.ItemHeight = 20;
+            this.BxEvents.Items.AddRange(new object[] {
+            "Anime ",
+            "Dance ",
+            "Dinner",
+            "Gaming ",
+            "Hiking",
+            "Movies",
+            "Soccer ",
+            "Tennis "});
+            this.BxEvents.Location = new System.Drawing.Point(32, 105);
+            this.BxEvents.Name = "BxEvents";
+            this.BxEvents.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.BxEvents.Size = new System.Drawing.Size(147, 124);
+            this.BxEvents.Sorted = true;
+            this.BxEvents.TabIndex = 3;
+            this.BxEvents.SelectedIndexChanged += new System.EventHandler(this.BxEvents_SelectedIndexChanged);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.ForeColor = System.Drawing.Color.Snow;
+            this.resultLabel.Location = new System.Drawing.Point(12, 354);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(66, 23);
+            this.resultLabel.TabIndex = 4;
+            this.resultLabel.Text = "Result";
+            this.resultLabel.Click += new System.EventHandler(this.resultLabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(28, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select An Event";
+            // 
+            // addActivityLabel
+            // 
+            this.addActivityLabel.AutoSize = true;
+            this.addActivityLabel.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addActivityLabel.ForeColor = System.Drawing.Color.Snow;
+            this.addActivityLabel.Location = new System.Drawing.Point(57, 256);
+            this.addActivityLabel.Name = "addActivityLabel";
+            this.addActivityLabel.Size = new System.Drawing.Size(143, 23);
+            this.addActivityLabel.TabIndex = 6;
+            this.addActivityLabel.Text = "Add an Activity";
+            this.addActivityLabel.Click += new System.EventHandler(this.addActivityLabel_Click);
+            // 
+            // Addbutton
+            // 
+            this.Addbutton.Location = new System.Drawing.Point(93, 298);
+            this.Addbutton.Name = "Addbutton";
+            this.Addbutton.Size = new System.Drawing.Size(107, 41);
+            this.Addbutton.TabIndex = 8;
+            this.Addbutton.Text = "Add an Item";
+            this.Addbutton.UseVisualStyleBackColor = true;
+            this.Addbutton.Click += new System.EventHandler(this.Addbutton_Click);
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(214, 298);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(107, 41);
+            this.RemoveButton.TabIndex = 9;
+            this.RemoveButton.Text = "Remove Item";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.comboBox1.Location = new System.Drawing.Point(241, 144);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Gender
+            // 
+            this.Gender.AutoSize = true;
+            this.Gender.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gender.ForeColor = System.Drawing.Color.Snow;
+            this.Gender.Location = new System.Drawing.Point(237, 115);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(74, 23);
+            this.Gender.TabIndex = 11;
+            this.Gender.Text = "Gender";
+            this.Gender.Click += new System.EventHandler(this.Gender_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.formatToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(416, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.colorToolStripMenuItem});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.formatToolStripMenuItem.Text = "F&ormat";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.helpToolStripMenuItem.Text = "&help";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.X)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.fontToolStripMenuItem.Text = "Fo&nt";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.colorToolStripMenuItem.Text = "&Color";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // ClubForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(416, 437);
+            this.Controls.Add(this.Gender);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.Addbutton);
+            this.Controls.Add(this.addActivityLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.BxEvents);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.addActivity);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ClubForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Computer Club Outing Sign Up";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox addActivity;
+        private System.Windows.Forms.Label ClubOutinglabel;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.ListBox BxEvents;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label addActivityLabel;
+        private System.Windows.Forms.Button Addbutton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label Gender;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class ClubForm : Form
+    {
+        public ClubForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addActivityLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BxEvents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string result = " ";
+
+            foreach (string activity in BxEvents.SelectedItems) {
+                result += activity + " ";
+            }
+               
+
+            textBoxResult.Text = result;
+
+        }
+
+        private void addActivity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Addbutton_Click(object sender, EventArgs e)
+        {
+            
+            BxEvents.Items.Add(addActivity.Text);
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            BxEvents.Items.Remove(addActivity.Text);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Gender_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resultLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
